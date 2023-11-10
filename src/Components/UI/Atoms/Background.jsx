1760ec2b8ebdfadc74a65f90185af79b858d5d97
rlasso@google.com <rlasso@google.com>
2023-11-09 20:43:38 -0500

    [Home][Hero] - Hero homepage develop
    - Hero component created
    - Container grid redesign

--- src/Components/UI/Atoms/Background.jsx
+++ src/Components/UI/Atoms/Background.jsx
@@ -1,4 +1,13 @@
 
-export const Background = () => {
-  return <div className="background"></div>;
+export const Background = ({ image = '', color = '' }) => {
+  const styles = {
+    backgroundColor: color,
+    backgroundImage: `url(${image})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  };
+
+  return (
+    <div className={`background`} style={styles}></div>
+  )
 }
--- src/Components/UI/Organisms/Hero.jsx
+++ src/Components/UI/Organisms/Hero.jsx
@@ -1,9 +1,21 @@
-import { TextButton } from "../Molecules/TextButton"
+import { Background } from "../Atoms/Background";
+import { Heading1 } from "../Atoms/Heading1"
 
 export const Hero = () => {
+  const blockClass = "hero";
+
   return (
-    <section className="hero">
-      <TextButton />
+    <section className={blockClass}>
+      <div className={`${blockClass}__wrapper container`}>
+        <Heading1
+          extraClass={`${blockClass}__title`}
+          text={'A decade of strategising and designing brands'}
+        />
+      </div>
+      <Background
+        image='https://fastly.picsum.photos/id/339/4752/3168.jpg?hmac=U_eb-zX4p02iRWKyDTGjkCUj6w42cRGinkHolnqPfuc'
+        color='#000'
+      />
     </section>
   )
 }
