c323dc2da18bec7374237054cb44374b210107b4
rlasso@google.com <rlasso@google.com>
2024-04-22 21:41:47 -0500

    Fixing build process

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { ReactRoutes } from './Routes/ReactRoutes';
-import { Background } from './Components/UI/Atoms/Background'
 import './Scss/main.scss';
 
 export const App = () => {
   return (
     <>
-      <Background />
       <ReactRoutes />
     </>
   )
